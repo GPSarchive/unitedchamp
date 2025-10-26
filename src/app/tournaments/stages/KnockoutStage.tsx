10ac844fbae77168fd8118d5ee0d06dedcc129dc
Giorgos Spyrou <SFnader@gmail.com>
2025-10-26 16:34:17 +0200

    update3

--- src/app/tournaments/stages/KnockoutStage.tsx
+++ src/app/tournaments/stages/KnockoutStage.tsx
@@ -3,7 +3,7 @@
 
 import React from "react";
 import type { Stage } from "../useTournamentData";
-import KOStageDisplay from "../koStage/KOStageDisplay";
+import KOStageDisplay from "./koStage/KOStageDisplay";
 
 const KnockoutStage: React.FC<{ stage: Stage }> = ({ stage }) => {
   return (
